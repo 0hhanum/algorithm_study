7859036074749e7ab4dcfaaf363de55838e98acf
hanum <rntls123@naver.com>
2022-02-23 00:06:46 +0900

    Baekjoon - Network#1922 [Kruskal, Union Find]

--- baekjoon/network_1922.py
+++ baekjoon/network_1922.py
@@ -1,3 +1,4 @@
+"""
 import heapq
 import sys
 
@@ -36,5 +37,41 @@ while counter < N - 1:
         counter += 1
         table[v1][v2] = 1
         table[v2][v1] = 1
+
+print(answer)
+"""
+import heapq
+import sys
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+root = [i for i in range(N)]
+heap = []
+counter = 0
+answer = 0
+
+for _ in range(M):
+    v1, v2, cost = list(map(int, sys.stdin.readline().split()))
+    heapq.heappush(heap, (cost, v1 - 1, v2 - 1))
+
+
+def find_root(x): # 이 함수를 통해 그룹 최신화
+    if root[x] != x:
+        root[x] = find_root(root[x])
+    return root[x]
+
+
+while counter < N - 1:
+    cost, v1, v2 = heapq.heappop(heap)
+    v1_root = find_root(v1)
+    v2_root = find_root(v2)
+
+    if v1_root != v2_root: # 같은 root 가 아니라면
+        answer += cost
+        counter += 1
+        if v1_root < v2_root:
+            root[v2_root] = v1_root
+        else:
+            root[v1_root] = v2_root
 
 print(answer)
