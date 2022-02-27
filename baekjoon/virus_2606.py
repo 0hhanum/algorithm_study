33738856c19b7ea8ef22aec53be461ed93489bb6
hanum <rntls123@naver.com>
2022-02-27 22:36:43 +0900

    BOJ - Virus#2606 [Union Find]

--- baekjoon/virus_2606.py
+++ baekjoon/virus_2606.py
@@ -1,3 +1,4 @@
+"""
 # BFS
 import sys
 from collections import defaultdict, deque
@@ -27,6 +28,49 @@ while queue:
             queue.append(v)
 
 print(counter - 1)
-
+"""
 
 # Union Find
+
+import sys
+
+V = int(sys.stdin.readline())
+E = int(sys.stdin.readline())
+
+root = [i for i in range(V + 1)]
+rank = [0 for i in range(V + 1)]
+
+
+def find(x):
+    if x != root[x]:
+        root[x] = find(root[x])
+        return find(root[x])
+    else:
+        return x
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x == y:
+        return
+    else:
+        rank_x = rank[x]
+        rank_y = rank[y]
+        if rank_x < rank_y:
+            root[x] = y
+        else:
+            root[y] = x
+            if rank_x == rank_y:
+                rank[x] += 1
+
+
+for _ in range(E):
+    v, w = list(map(int, sys.stdin.readline().split()))
+    union(v, w)
+
+for i in range(1, V + 1):  # root 최신화
+    find(i)
+    # 이래도 안되는 경우 있을거같음. find(1) 과 같은걸 count 하는 방법이 나아보임.
+R = root[1]
+print(root.count(R) - 1)
