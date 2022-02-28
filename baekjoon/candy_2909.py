3e635266183c2dd925a8fb8a07dcbe41ff3cf024
hanum <rntls123@naver.com>
2022-03-01 00:17:13 +0900

    BOJ - Candy#2909

--- baekjoon/candy_2909.py
+++ baekjoon/candy_2909.py
@@ -1,9 +1,19 @@
 import sys
 
 price, zero = sys.stdin.readline().split()
+
 zero = int(zero)
-if zero == 0:
+if zero == 0 or price == "0":
     print(price)
+elif len(str(price)) < zero:
+    print("0")
+elif len(str(price)) == zero:
+    price = str(price)
+    if price[0] < "5":
+        print("0")
+    else:
+        print("1" + "0" * zero)
+
 else:
     target = int(price[-zero])
     if target >= 5:
