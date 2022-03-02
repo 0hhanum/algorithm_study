4cfac311b0a3223581f30c800dce21a4940013b0
hanum <rntls123@naver.com>
2022-03-03 01:37:36 +0900

    BOJ - Binary Tree#5639 - 2

--- baekjoon/binary_tree_5639.py
+++ baekjoon/binary_tree_5639.py
@@ -1,14 +1,26 @@
 import sys
 
+sys.setrecursionlimit(10 ** 7)  # 재귀 제한 풀기
 
 class Tree:
     def __init__(self, nodes):
         self.root = nodes[0]
-        _left = list(filter(lambda x: x < self.root, nodes))
-        _right = list(filter(lambda x: x > self.root, nodes))
+        # _left = list(filter(lambda x: x < self.root, nodes))
+        # _right = list(filter(lambda x: x > self.root, nodes))
 
-        self.left = Tree(_left) if _left else None
-        self.right = Tree(_right) if _right else None
+        # self.left = Tree(_left) if _left else None
+        # self.right = Tree(_right) if _right else None
+        _left = []
+        _right = []
+        index = -1
+        for index, node in enumerate(nodes[1:]):
+            if node > self.root:
+                break
+
+        _left = nodes[1:index + 1]
+        _right = nodes[index + 1:]
+        self.left = Tree(_left) if _left and index != -1 else None
+        self.right = Tree(_right) if _right and index != -1 else None
 
 
 def go(tree):
@@ -20,8 +32,10 @@ def go(tree):
 
 
 nodes = []
-inputs = sys.stdin.readlines()
-for i in inputs:
-    nodes.append(int(i))
+# inputs = sys.stdin.readlines()  # 입력 값 개수 안알려줄 때 hack
+# for i in inputs:
+#     nodes.append(int(i))
+for i in range(9):
+    nodes.append(int(sys.stdin.readline()))
 tree = Tree(nodes)
 go(tree)
