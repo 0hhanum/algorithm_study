715003d5da476d9de7f5d29cdb46dcc3b20ff47c
hanum <rntls123@naver.com>
2022-03-04 02:09:26 +0900

    Programmers - Keep Distance#2

--- programmers/keep_distance.py
+++ programmers/keep_distance.py
@@ -10,24 +10,26 @@ def solution(places):
                     finder(place, (i, j), i, j, memo, 0, result)
                     if result:
                         tmp = True
-                        answer.append(1)
+                        answer.append(0)
                         break
             if tmp:
                 break
         if not tmp:
-            answer.append(0)
+            answer.append(1)
     return answer
 
 
 def finder(graph, start, x, y, memo, count, result):
+    if x < 0 or y < 0:
+        return
     if (x, y) == start:  # 사방으로 보내기
         memo[x][y] = 1
         count += 1
-        finder(graph, start, x - 1, y - 1, memo, count, result)
-        finder(graph, start, x - 1, y + 1, memo, count, result)
-        finder(graph, start, x + 1, y - 1, memo, count, result)
-        finder(graph, start, x + 1, y + 1, memo, count, result)
-
+        finder(graph, start, x, y - 1, memo, count, result)
+        finder(graph, start, x, y + 1, memo, count, result)
+        finder(graph, start, x + 1, y, memo, count, result)
+        finder(graph, start, x - 1, y, memo, count, result)
+        return
     if result or count > 2:
         # 결과가 나왔거나, 2칸 이상 이동했거나, 이미 방문한 점이면 종료
         return
@@ -39,7 +41,6 @@ def finder(graph, start, x, y, memo, count, result):
     if visited:
         return
     memo[x][y] = 1
-    count += 1
 
     if current == "P" and count <= 2:  # hit
         result.append(1)
@@ -47,8 +48,8 @@ def finder(graph, start, x, y, memo, count, result):
     elif current == "X":
         return
     else:  # 비어있는 경우 -> 사방으로 보낸다
-        finder(graph, start, x - 1, y - 1, memo, count, result)
-        finder(graph, start, x - 1, y + 1, memo, count, result)
-        finder(graph, start, x + 1, y - 1, memo, count, result)
-        finder(graph, start, x + 1, y + 1, memo, count, result)
-print(solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]))
+        count += 1
+        finder(graph, start, x, y - 1, memo, count, result)
+        finder(graph, start, x, y + 1, memo, count, result)
+        finder(graph, start, x + 1, y, memo, count, result)
+        finder(graph, start, x - 1, y, memo, count, result)
