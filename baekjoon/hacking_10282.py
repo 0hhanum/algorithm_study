918f46d8b7c502100f4139ffb10a6a4b0389f748
hanum <rntls123@naver.com>
2022-03-17 21:17:24 +0900

    BOJ - Hacking#10282 [다익스트라]

---
+++ baekjoon/hacking_10282.py
@@ -0,0 +1,35 @@
+import heapq
+import sys
+from collections import defaultdict
+
+N = int(sys.stdin.readline().strip())
+
+for _ in range(N):
+    n, d, c = map(int, sys.stdin.readline().strip().split())
+    times = [float('inf') for _ in range(n + 1)]
+    times[c] = 0
+    heap = [[0, c]]
+    table = defaultdict(list)
+    maximum = 0
+    counter = 1
+    for _ in range(d):
+        a, b, s = map(int, sys.stdin.readline().strip().split())
+        table[b].append([s, a])
+
+    while heap:
+        current_time, current = heapq.heappop(heap)
+        if current_time > times[current]:
+            continue
+        if table[current]:
+            for target_time, target in table[current]:
+                total_time = current_time + target_time
+                if total_time < times[target]:
+                    if times[target] == float('inf'):
+                        counter += 1
+                    times[target] = total_time
+                    heapq.heappush(heap, [total_time, target])
+    for time in times:
+        if time == float('inf'):
+            continue
+        if time > maximum:
+            maximum = time
